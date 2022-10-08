1ff72ed5ec51da1ca5d4de2c708cf08685f2cf17
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2022-10-08 15:36:29 +0900

    ifrendering

---
+++ src/views/IfRendering.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <!-- === 자료형까지 비교  -->
+        <!-- v-if
+            1. 태그 자체가 사라진다.
+            2. 그릴 때 시간이 조금 더 오래 걸린다.  -->
+        <p v-if="renderType === 1" id="1">
+            현재 renderType은 1입니다.
+        </p>
+        <p v-else-if="renderType === 2" id="2">
+            현재 renderType은 2입니다.
+        </p>
+        <p v-else id="3">
+            renderType: {{renderType}}
+        </p>
+
+        <!-- v-model.number 자료형을 number로 설정해준다.  -->
+        <p> 
+            <input type="text" v-model.number="renderType" />
+        </p>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            renderType: 1,
+        };
+    },
+}   
+</script>
---
+++ src/views/ShowRenderingType.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <!-- v-show 는 show hide 방식 
+        탭같은 애들은 show hide 로 하는게 훨 낫지. 데이터가 없어지지 않도록! 
+         -->
+        <p v-show="renderType2 === 1" id="1">
+            현재 renderType2은 1입니다.
+        </p>
+        <p v-show="renderType2 === 2" id="2">
+            현재 renderType2은 2입니다.
+        </p>
+
+        <!-- v-model.number 자료형을 number로 설정해준다.  -->
+        <p> 
+            <input type="text" v-model.number="renderType2" />
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            renderType2: 1,
+        };
+    },
+}   
+</script>
