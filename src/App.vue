63ab116ff8428213332dee93cbb9151a1af34475
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2022-10-15 17:57:54 +0900

    props 수업 진행중

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <!-- 1-1. props : 부모 컴포넌트가 가지고 있는 데이터를 자식에게 넘겨주는것. -->
+      <!-- 1-3. 여기서 v-bind 형태로 넣어주고..  -->
+      <!-- 3-1. 바인딩 해주지 않으면 특히 숫자 형을 제대로 읽어오지 못하는거같아 :message="message" -->
+     <ChildView
+        :message="message"
+     ></ChildView>
+     <input type="text" v-model="message">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChildView from './views/ChildView.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ChildView
+  },
+  data(){
+    return {
+      // 1-2. 부모가 가진 message
+      message: "Hello World",
+    }
   }
 }
 </script>
---
+++ src/views/ChildView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>자식 컴포넌트 입니다. </h2>
+    <!-- 1-5. 여기서 사용! -->
+    <!-- 2-1. default를 정해야하는 이유 : 부모 컴포넌트에서 값을 바인딩 해주지 않으면 오류가 생길 여지가 있음 -->
+    <p>{{message}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    // 1-4. 여기 props 에서 불러온 후 type과 default를 지정하면 컴포넌트에서 사용할 수 있다.
+    props : {
+        message : {
+            type: String,
+            default: "안녕하세요",
+            require: true,  // 필수로 들어가야하는값(경고만 내지만 오류는 아님)
+            // validator(value) {
+            //     return value === 1;
+            // }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
