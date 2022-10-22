c589749bea647133c37227772acac60138726690
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2022-10-22 17:52:35 +0900

    mixin 진행중

---
+++ src/views/AboutView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    {{ wrapData }}
+  </div>
+</template>
+
+<script>
+  import mixin from '@/mixin';
+
+  export default{
+    mixins : [mixin],
+    data() {
+      return {
+        // 1000 : 1초
+        // * 60 : 1분
+        // * 60 : 1시간
+        // * 24 : 하루
+        yesterDay: new Date(new Date().getTime() - 1000 * 60 * 60 * 24),
+      }
+    },
+    computed: {
+      wrapData() {
+        return this.toTmdHms(this.yesterDay);
+      }
+    }
+  }
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    {{ wrapData }}
+    <hr />
+    {{ message }}
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import mixin from '@/mixin';
+
+export default {
+  mixins : [mixin],
+  components: {
+  },
+  data(){
+    return {
+      now: new Date(),
+    }
+  },
+
+  computed: {
+    wrapData() {
+      return this.toTmdHms(this.now);
+    }
+  }
+}
+</script>
