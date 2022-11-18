4259525f9cefc8481a46ddac70e24d5efff3f511
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2022-11-19 00:37:54 +0900

    로그아웃 추가

--- src/views/layoutView/LayoutView.vue
+++ src/views/layoutView/LayoutView.vue
@@ -1,55 +1,26 @@
 <template>
   <div>
-    <v-app-bar
-      absolute
-      color="white"
-      elevate-on-scroll
-      scroll-target="#scrolling-techniques-7"
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-    </v-app-bar>
-    <v-navigation-drawer v-model="drawer" absolute temporary>
-      <v-list-item>
-        <v-list-item-avatar>
-          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
-        </v-list-item-avatar>
+    <div class="app-header">
+      <button class="btn-logout" @click="logout">로그아웃</button>
+    </div>
 
-        <v-list-item-content>
-          <v-list-item-title>John Leider</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" :to="item.to">
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-container>
+    <div class="router-container">
       <router-view></router-view>
-    </v-container>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
-  data() {
-    return {
-      drawer: null,
-      items: [
-        { title: "내 일기장", icon: "home", to: "/" },
-        { title: "다른사람 일기장", icon: "mdi-forum", to: "/other" },
-      ],
-    };
+  methods: {
+    ...mapActions({ setToken: "user/setToken" }),
+    logout() {
+      if (confirm("로그아웃 하시겠습니까?")) {
+        this.setToken("");
+      }
+    },
   },
 };
 </script>
